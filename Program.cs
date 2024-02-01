97dd34191d1a49413ec1357755a9af2bb4cc4672
manjulachukki <55753426+manjulachukki@users.noreply.github.com>
2024-02-01 15:09:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TeamcityProject1WithoutTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = Add(num1, num2);
+            Console.WriteLine($"The sum is: {sum}");
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
